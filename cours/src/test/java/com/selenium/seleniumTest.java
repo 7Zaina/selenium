8a3766a67d8bf994101ebef72a43c6dfd20fa5f6
arezki <arezki.guezout@gmail.com>
2025-07-17 13:09:57 +0100

    premier test avec selenium

--- cours/src/test/java/com/selenium/seleniumTest.java
+++ cours/src/test/java/com/selenium/seleniumTest.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class test_selenium {
+public class seleniumTest {
     
     @Test
     public void test(){
